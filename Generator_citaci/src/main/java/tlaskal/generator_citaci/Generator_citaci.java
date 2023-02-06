66a68ff8ca57dea8c675f5377897a825c616a943
Tobiáš Tláskal <tobias.tlaskal@tul.cz>
2023-02-06 19:28:09 +0100

    Funkce Menu
    Přidány funkce třídy Menu. Dokončena funkčnost úvodního menu. Drobné změny ve třídách Export a Menu. Hlavní třída obsahuje testovací kód. Přidán javadoc.

--- Generator_citaci/src/main/java/tlaskal/generator_citaci/Generator_citaci.java
+++ Generator_citaci/src/main/java/tlaskal/generator_citaci/Generator_citaci.java
@@ -1,16 +1,22 @@
 package tlaskal.generator_citaci;
 
+import java.net.URL;
+
 /**
- * Semestrální projekt předmětu Objektové programování - CLI generátor citací 
+ * Semestrální projekt předmětu Objektové programování - CLI generátor citací
+ *
  * @author Tobiáš Tláskal
  */
 public class Generator_citaci {
 
+    static Menu menu = new Menu();
+
     /**
      * Metoda main. Spouští se při zapnutí programu.
+     *
      * @param args parametry z příkazového řádku ve formě pole
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        switch (menu.getMenuUvod(3)) {
     }
 }
